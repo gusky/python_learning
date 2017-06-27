a235f15c24a5524fcc1d0bf0eba322dadc24d6fb
gusky <504935518@qq.com>
2017-06-28 00:11:47 +0800

    Update basic.py

--- samples/basic/basic.py
+++ samples/basic/basic.py
@@ -1,9 +1,18 @@
 print('hello,python')
 
+
 name=input('Please input your name:')
 print("hello,",name)
+
 
 a='ABC'
 b=a
 a='XYZ'
 print(b)  #get 'ABC'
+
+
+list(range(5)) #得到结果为：[0, 1, 2, 3, 4]
+#               #注意此时list后面要用小括号()
+
+a=list(range(0,100)) #注意list后面加小括号（）而不是中括号[]
+print(a)
